75baf2477b1dba49554b5ccdca1c0cc8c99adbc4
Ubiratan <r14.ubiratanjunior@gmail.com>
2021-12-24 16:24:02 -0300

    [+] last exercise implementation

---
+++ cap03/05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type ubiratan int
+
+var x ubiratan
+var y int
+
+func main() {
+	fmt.Printf("%v\t%v\n", x, y)
+	x = 42
+	fmt.Printf("%v\n", x)
+
+	y = int(x)
+	fmt.Printf("%v %T", y, y)
+}
